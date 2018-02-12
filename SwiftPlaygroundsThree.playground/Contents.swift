897cc03d04d1047f6116161fd2d84cae875d4461
cmquiamz <cmquiamz@hawaii.edu>
2018-02-12 10:39:04 -1000

    Finished Assignment #4

--- SwiftPlaygroundsThree.playground/Contents.swift
+++ SwiftPlaygroundsThree.playground/Contents.swift
@@ -3,3 +3,44 @@
 import UIKit
 
 var str = "Hello, playground"
+
+var fruitBasket: [String]
+
+var simpleString: String
+
+//Initialize an aray using an Array literal
+
+fruitBasket =  ["Pear", "Apple", "Mango", "Pineapple"]
+print(fruitBasket)
+
+//For-in loop
+for name in fruitBasket{
+    //print "I like to eat" before every name of fruit in fruitBasket
+    
+    print("I like to eat " + name)
+    
+}
+
+//let's print out item at index 1
+
+print(fruitBasket[1])
+print(fruitBasket[0])
+
+
+
+
+//Part 8, Start of Assignment #4
+
+var theNumbers: [Int]
+theNumbers = [0,1,2,3,4]
+print(theNumbers)
+
+for name in theNumbers{
+    print("My favorite number ", name)
+}
+
+let removed = theNumbers.remove(at: 2)
+print(theNumbers)
+
+theNumbers.append(5)
+print(theNumbers)
